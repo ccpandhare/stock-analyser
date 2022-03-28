aa88e1f5f9c5f6d19c9680f4a241c38a302de81d
AshutoshSancheti <ashusancheti13@gmail.com>
2022-03-28 22:17:43 +0530

    Added requirements.txt; Updated the code of class CompanyTickers to fetch the URL and ticker from the tables in screen webpages;

--- lib/utils/classes.py
+++ lib/utils/classes.py
@@ -2,8 +2,10 @@ import datetime
 import argparse
 import requests
 import pandas as pd
-from ast import literal_eval
 from os import path
+from bs4 import BeautifulSoup
+import re
+from time import sleep
 
 class Document:
     name: str
@@ -27,14 +29,18 @@ class Stock:
     def __init__(ticker: str):
         self.ticker = ticker
 
+    def DCF_MC_analysis():
+        
 
 
-class myLoginSession:
+
+class Session:
     """
     Usage: 
-    LS = myLoginSession('user', 'pass')
+    LS = Session('user', 'pass')
     session = LS.mySession()
-
+    ....
+    LS.close() or session.close()
     """
     __username: str
     __password: str
@@ -43,7 +49,7 @@ class myLoginSession:
 
     __session: [requests.sessions.Session]
 
-    def __init__(self, username, password):
+    def __init__(self, username: str, password: str):
         #TODO: Remove un necessary stuff. See if we need to store theusername and password in class variables
         self.__username = username
         self.__password = password
@@ -66,10 +72,10 @@ class myLoginSession:
 
 
 
-class Companies:
+class CompanyTickers:
     """
     Usage:
-    Companies = Companies(session, 101)
+    Companies = Companies(session=session,limit=101)
     myCompanies = Companies.getCompanyTickers()
     """
     __screen_url: str = 'https://www.screener.in/screen/raw/'
@@ -83,97 +89,52 @@ class Companies:
     def __init__(self,session,limit=100000):
         self.__limit = limit
         self.__session = session
-        #self.__companies = pd.DataFrame() #empty DF to append to
         
-    def setLimit(self,limit=100000, refetch = False):
+    def setLimit(self,limit=100000, forceRefetch = False):
         self.__limit = limit
-        if(refetch == True):
+        if(forceRefetch == True):
             self.__fetchCompanies()
 
     def __fetchCompanies(self):
         screen_referer_url = self.__screen_referer_url
         screen_payload = {'sort':'','source':'','query': 'Current price '}
 
-        companies_df = pd.DataFrame() #empty DF to append to
+        companyTickers = pd.DataFrame(columns = ['Name', 'Ticker', 'URL']) #empty DF to append to
 
         page_iter = 1
-        row_count = -1
+        row_iter = 0
+        row_count = 50
         while(True):
             # Get the webpage after submitting the query to screen page
             screen_page = self.__session.get(self.__screen_url, params = screen_payload, headers=dict(Referer=screen_referer_url))
             #print(screen_page.url) #Prints the URL of the new web page
 
-            # Extract the table into dataframe. skiprows is used to remove the un necessary rows
-            table = pd.read_html(screen_page.text, index_col = 0, header = 0, skiprows = [16, 32, 48])[0] #table is a list
-            no_of_rows = table.shape[0]
+            screen_page_soup = BeautifulSoup(screen_page.text, 'html.parser')
+            screen_table = screen_page_soup.findAll('table')[0] #Extracting the 1st HTML table from the list
 
-            # Updating the row_count using the number of rows from 1st page
-            if(row_count == -1):
-                row_count = no_of_rows
-                if(row_count == 0):
-                    return #TODO: Need to add a error here
-            
-            companies_df = pd.concat([companies_df, table], ignore_index = False)
+            for anchor in screen_table.findAll('a', target="_blank"):
+                company_name = re.findall("\w.+", anchor.text)[0]
+                company_url = anchor['href']
+                company_ticker = company_url.split('/')[2]
+                row_iter = row_iter + 1
+                companyTickers.loc[row_iter] = [company_name, company_ticker, company_url]
+                
             page_iter = page_iter + 1
-
             screen_referer_url = screen_page.url
-            if(no_of_rows != row_count):
+            if(row_iter%row_count != 0):
                 break
-            elif(companies_df.shape[0] >= self.__limit):
-                companies_df = companies_df.loc[companies_df.index <= (self.__limit)]
+            elif(row_iter >= self.__limit):
+                #companyTickers.set_index('No.', inplace=True)
+                companyTickers = companyTickers.loc[companyTickers.index <= (self.__limit)]
                 break
             else:
                 screen_payload['page'] = str(page_iter)
             sleep(1)
-        companies_df.to_csv('../../Companies.csv')
-
-        #The companyTickers are indexed from 1 while companies_df are 0-indexed
-        companyTickers = pd.DataFrame(index = range(1, companies_df.shape[0]+1), columns = ['Name', 'Ticker', 'URL'])
-        # Can use companyTickers.index.rename('S.No.', inplace=True) to rename the index col
-        # & companyTickers = companyTickers.reindex(range(0,90))
-
-        # Extract the company name and the company URL sub-path
-        i = 1
-        while(i<=companies_df.shape[0]):
-        #for i in range(1, companies_df.shape[0]+1):
-            # i-1 is used for the values index as companies_df are 0-indexed
-            company_payload = {'q':(companies_df['Name'].values[i-1]), 'v':'2'}
-
-            try:
-                # Provides the dictionary containing company info using the company_payload
-                company_search = session.get(self.__company_search_url, params = company_payload)
-
-                # TODO: Check if this can be done in a better way
-                company_dict = literal_eval(company_search.text[1:-1])
-                # The company_serach can provide a tuple of dictionaries containing companies info
-                # which match the  using the company_payload companies_df['Name'] value but we pick 1st
-                # Ex: companies_df['Name'].values[i-1] = "Marico", then the company_search query would 
-                # provide Marico Ltd' & 'Marico Kaya Enterprises Ltd'
-                if(type(company_dict) == type(())):
-                    company_dict = company_dict[0]
-                company_name = company_dict['name']
-                company_url = company_dict['url']
-                company_ticker = company_url.split('/')[2]
-                companyTickers.loc[i] = [company_name, company_ticker, company_url]
-                # companyTickers['Name'][i] = company_name
-                # companyTickers['Ticker'][i] = company_ticker
-                # companyTickers['URL'][i] = company_url
-            except:
-                # TODO : Need to find a graceful way of cleaning up the connection
-                sleep(5)
         companyTickers.to_csv('../../CompanyTickers.csv')
-    
-    def getCompanies(self,refetch = False) -> [pd.DataFrame]:
-        if((refetch == True) or (path.exists('../../Companies.csv') == False)):
-            self.__fetchCompanies()
-        companies = pd.read_csv('../../Companies.csv', index_col = 0)
-        if(companies.shape[0] != self.__limit):
-            self.__fetchCompanies()
-            companies = pd.read_csv('../../Companies.csv', index_col = 0)
-        return companies
+        self.__companyTickers = companyTickers
 
-    def getCompanyTickers(self,refetch = False) -> [pd.DataFrame]:
-        if((refetch == True) or (path.exists('../../CompanyTickers.csv') == False)):
+    def getCompanyTickers(self,forceRefetch = False) -> [pd.DataFrame]:
+        if((forceRefetch == True) or (path.exists('../../CompanyTickers.csv') == False)):
             self.__fetchCompanies()
         companyTickers = pd.read_csv('../../CompanyTickers.csv', index_col = 0)
         if(companyTickers.shape[0] != self.__limit):
